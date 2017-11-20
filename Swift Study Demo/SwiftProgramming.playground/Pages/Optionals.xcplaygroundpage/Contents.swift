5aa752cb5079b291d843f8c1e8d4d960ad59d07b
JianCui <ccjiancui@gmail.com>
2017-11-20 17:58:42 +0800

    Update optional Demo

--- Swift Study Demo/SwiftProgramming.playground/Pages/Optionals.xcplaygroundpage/Contents.swift
+++ Swift Study Demo/SwiftProgramming.playground/Pages/Optionals.xcplaygroundpage/Contents.swift
@@ -4,4 +4,11 @@ import Foundation
 
 var str = "Hello, playground"
 
+// 1.optional type 可选类型
+// 2.optional binding 可选绑定
+// 3.implicityly unwrapped optional 隐式强制解析
+// 4.optional chaining 可选链式调用
+// 5.原地修改可控实例 append(_:)
+// 6. ?? 空合运算符
+
 //: [Next](@next)
