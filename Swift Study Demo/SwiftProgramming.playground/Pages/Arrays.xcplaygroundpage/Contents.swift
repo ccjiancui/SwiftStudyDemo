3102351a1051c919c46232cc0fcd1b312424c982
JianCui <ccjiancui@gmail.com>
2018-12-14 16:51:22 +0800

    update

--- Swift Study Demo/SwiftProgramming.playground/Pages/Arrays.xcplaygroundpage/Contents.swift
+++ Swift Study Demo/SwiftProgramming.playground/Pages/Arrays.xcplaygroundpage/Contents.swift
@@ -2,6 +2,19 @@
 
 import Foundation
 
+//var numbers = [1, 2, 3, 4, 5]
+//numbers.remove(at: 1)
+//numbers
+
 var str = "Hello, playground"
+
+let numbers: [[Any]] = [[1, 2, 3], ["5", 6, 7]]
+let num = numbers.flatMap { (num) -> [Any] in
+    return num
+}.compactMap { (num) -> Int? in
+    return num as? Int
+}
+num
+
 
 //: [Next](@next)
